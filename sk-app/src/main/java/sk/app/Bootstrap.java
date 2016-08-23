c99af0960de44c0a8c0e34dbc8a86406557fba67
jcruz <jcruz@jcruz>
2016-08-23 08:15:24 -0300

    ajuste no core

--- sk-app/src/main/java/sk/app/Bootstrap.java
+++ sk-app/src/main/java/sk/app/Bootstrap.java
@@ -1,14 +1,19 @@
 package sk.app;
 
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import sk.impl.Beans;
+
 public class Bootstrap {
 	public static void main(String[] args) throws Exception {
-		/*
+
 		Weld weld = new Weld();
 		WeldContainer container = weld.initialize();
 		Beans.setBeanManager(container.getBeanManager());
 		Bootstrap mainApp = container.instance().select(Bootstrap.class).get();
 		mainApp.start(args);
-		*/
+
 		System.out.println(System.getProperty("user.dir"));
 		System.out.println(args[0]);
 	}
