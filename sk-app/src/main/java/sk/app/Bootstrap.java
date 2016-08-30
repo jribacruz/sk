c7c99efab92fecbce4e1bb63be92e2136bd7eb5d
jcruz <jcruz@jcruz>
2016-08-30 20:34:00 -0300

    ajuste no core

--- sk-app/src/main/java/sk/app/Bootstrap.java
+++ sk-app/src/main/java/sk/app/Bootstrap.java
@@ -3,22 +3,30 @@ package sk.app;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
+import sk.api.util.Colorize;
 import sk.impl.Beans;
 
 public class Bootstrap {
+
 	public static void main(String[] args) throws Exception {
+		Bootstrap.validateApp(args);
 
 		Weld weld = new Weld();
 		WeldContainer container = weld.initialize();
 		Beans.setBeanManager(container.getBeanManager());
 		Bootstrap mainApp = container.instance().select(Bootstrap.class).get();
 		mainApp.start(args);
-
-		System.out.println(System.getProperty("user.dir"));
-		System.out.println(args[0]);
+		container.close();
 	}
 
 	private void start(String args[]) throws Exception {
 
+	}
+
+	private static void validateApp(String[] args) {
+		if (args.length == 0) {
+			System.out.println(Colorize.red("É necessário indicar o caminho de um projeto maven como parametro."));
+			System.exit(0);
+		}
 	}
 }
