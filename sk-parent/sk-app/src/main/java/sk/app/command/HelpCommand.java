1a8d898d8d8764835598832733b868787bf248f7
jcruz <jcruz@jcruz>
2016-09-05 18:58:27 -0300

    ajuste no help

--- sk-parent/sk-app/src/main/java/sk/app/command/HelpCommand.java
+++ sk-parent/sk-app/src/main/java/sk/app/command/HelpCommand.java
@@ -2,6 +2,8 @@ package sk.app.command;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.StringUtils;
+
 import sk.api.annotation.CommandConf;
 import sk.api.command.Command;
 import sk.api.command.CommandContext;
@@ -15,8 +17,13 @@ public class HelpCommand implements Command {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void execute(CommandContext cmdContext) throws IOException {
-
+	public void execute(CommandContext commandContext) throws IOException {
+		int rightPad = commandContext.getCommands().keySet().stream().mapToInt(String::length).reduce(Integer::max).getAsInt();
+		System.out.println("");
+		commandContext.getCommands().forEach((key, command) -> {
+			System.out.println(String.format(" %s - %s", StringUtils.rightPad(command.getUUID(), rightPad), command.getDescription()));
+		});
+		System.out.println("");
 	}
 
 }
