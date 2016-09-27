ff42d02611fb275c3a62e8555029483fd04f7717
jcruz <jcruz@jcruz>
2016-09-27 15:38:28 -0300

    ajuste no XMLParser

--- sk-parent/sk-impl/src/main/java/sk/impl/util/XMLParser.java
+++ sk-parent/sk-impl/src/main/java/sk/impl/util/XMLParser.java
@@ -2,19 +2,31 @@ package sk.impl.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import sk.api.util.Colorize;
 
 public class XMLParser {
 
 	private File xmlFile;
+
+	private XPath xpath;
+
+	private Document doc;
 
 	public XMLParser(File xmlFile) throws SAXException, IOException, ParserConfigurationException {
 		super();
@@ -25,9 +37,22 @@ public class XMLParser {
 	private void init() throws SAXException, IOException, ParserConfigurationException {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document doc = dBuilder.parse(this.xmlFile);
+		this.doc = dBuilder.parse(this.xmlFile);
 		XPathFactory xPathfactory = XPathFactory.newInstance();
-		XPath xpath = xPathfactory.newXPath();
+		this.xpath = xPathfactory.newXPath();
+	}
+
+	public List<Node> getNodesByXPathExpression(String expression) {
+		List<Node> nodes = new ArrayList<>();
+		try {
+			NodeList nodeList = (NodeList) this.xpath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				nodes.add(nodeList.item(i));
+			}
+		} catch (XPathExpressionException e) {
+			System.out.println(Colorize.red("Erro: " + e.getMessage()));
+		}
+		return nodes;
 	}
 
 }
---
+++ sk-parent/sk-impl/src/test/java/sk/impl/XMLParserTestCase.java
@@ -0,0 +1,22 @@
+package sk.impl;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import sk.impl.util.XMLParser;
+
+public class XMLParserTestCase {
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+		XMLParser parser = new XMLParser(new File("src/test/java/resources/persistence.xml"));
+
+		//@formatter:off
+		parser.getNodesByXPathExpression("//property")
+			.stream()
+			.forEach(p -> System.out.println(p.getAttributes().getNamedItem("value").getTextContent()));
+		//@formatter:on
+	}
+}
