3c71a6654727c9c1835f67b3b24590fa25c995bb
jribacruz <jribacruz@gmail.com>
2016-10-17 21:29:41 -0300

    ajustes no pom.xml

--- sk-parent/sk-app/src/main/resources/bin/sk.sh
+++ sk-parent/sk-app/src/main/resources/bin/sk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath ${HOME}/sk/libs/*:${HOME}/sk/modules/*:${HOME}/sk/sk-app-${project.version}.jar sk.app.Bootstrap $1
+java -classpath ${HOME}/git/sk.github.io/libs/*:${HOME}/git/sk.github.io/modules/*:${HOME}/git/sk.github.io/sk-app-${project.version}.jar sk.app.Bootstrap $1
